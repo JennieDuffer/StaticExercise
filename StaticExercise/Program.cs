508e1a710ee80b0e05b10b49fc7413e8a3251859
Jennie Duffer <jenniej297@gmail.com>
2022-08-22 11:16:03 -0500

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,7 @@
+﻿using StaticExercise;
+
+TempConverter.FahrenheitToCelsius(32);
+Console.WriteLine(TempConverter.FahrenheitToCelsius(32));
+
+TempConverter.CelsiusToFahrenheit(0);
+Console.WriteLine(TempConverter.CelsiusToFahrenheit(0));
